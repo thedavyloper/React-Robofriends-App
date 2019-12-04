6e952a997080a35cc417432992d164753b36c1b4
David Akpan <daviduwana@gmail.com>
2019-12-04 16:12:59 +0100

    Updates

---
+++ precache-manifest.9568b851b557e84f597c5e7a14894674.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4aeec0c4ea4f237e503410f9da7cf5b9",
+    "url": "/React-Robofriends-App/index.html"
+  },
+  {
+    "revision": "00fde845e344dad5fb56",
+    "url": "/React-Robofriends-App/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "d11deb11529f1bcc7fad",
+    "url": "/React-Robofriends-App/static/css/main.1d6cd40b.chunk.css"
+  },
+  {
+    "revision": "00fde845e344dad5fb56",
+    "url": "/React-Robofriends-App/static/js/2.6fb0d3a3.chunk.js"
+  },
+  {
+    "revision": "d11deb11529f1bcc7fad",
+    "url": "/React-Robofriends-App/static/js/main.5e1163a3.chunk.js"
+  },
+  {
+    "revision": "d081c15d6df85086a428",
+    "url": "/React-Robofriends-App/static/js/runtime~main.4d8b9493.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/React-Robofriends-App/static/media/SEGA.12a3db8b.woff"
+  }
+]);
